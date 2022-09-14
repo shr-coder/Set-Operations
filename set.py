a9a6a436125220115419f2a8f19e1447d853d19f
shr-coder <85057063+shr-coder@users.noreply.github.com>
2022-09-15 00:22:00 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,6 @@
+E = {1, 2, 4, 3, 7};
+N = {1, 2, 3, 0, 8};
+print("Union of E and N is",E | N)
+print("Intersection of E and N is",E & N)
+print("Difference of E and N is",E - N)
+print("Symmetric difference of E and N is",E ^ N)
